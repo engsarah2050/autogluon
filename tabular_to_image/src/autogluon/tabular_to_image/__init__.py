9a791669fe4a0ff48d0331013279043ac1c4a33b
engsarah2050 <eng.sarah2050@gmail.com>
2022-03-26 15:31:24 +0200

    work42

--- tabular_to_image/src/autogluon/tabular_to_image/__init__.py
+++ tabular_to_image/src/autogluon/tabular_to_image/__init__.py
@@ -2,7 +2,7 @@ import logging
 
 from autogluon.core.dataset import TabularDataset
 from autogluon.common.features.feature_metadata import FeatureMetadata
-#from autogluon.tabular_to_image.utils_pro import Utils_pro
+from autogluon.tabular_to_image.utils_pro import Utils_pro
 from autogluon.tabular_to_image.models_zoo import ModelsZoo
 from autogluon.DeepInsight_auto.pyDeepInsight import ImageTransformer,LogScaler
 
