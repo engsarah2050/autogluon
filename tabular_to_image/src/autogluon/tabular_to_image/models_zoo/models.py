d6c437f405fda03cfa9e45c5db1e0c84fdd5a4b0
engsarah2050 <eng.sarah2050@gmail.com>
2022-10-09 00:13:03 +0200

    work295

--- tabular_to_image/src/autogluon/tabular_to_image/models_zoo/models.py
+++ tabular_to_image/src/autogluon/tabular_to_image/models_zoo/models.py
@@ -1236,7 +1236,18 @@ class ModelsZoo():
                         model = models.efficientnet_b1(weights=(weights,pretrained)).to(device) 
                         for param in model.parameters():
                             param.requires_grad =True                     
-                        model._fc = nn.Linear(model.classifier.in_features,self.N_class).to(device)   
+                        classifier =nn.Sequential(
+                                    nn.Flatten(),
+                                    nn.Linear(in_features=model.classifier.in_features, out_features=512, bias=True),
+                                    nn.BatchNorm1d(512, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                                    nn.ReLU(inplace=True), 
+                                    nn.Linear(in_features=512, out_features=256, bias=True),
+                                    nn.BatchNorm1d(256, eps=1e-05, momentum=0.1, affine=True, track_running_stats=True),
+                                    nn.ReLU(inplace=True), 
+                                    nn.Linear(in_features=256, out_features=self.N_class, bias=True),
+                                    nn.LogSoftmax(dim=1) ,
+                                    )
+                        model.classifier = classifier 
         elif int(self.ImageShape)==self.commonShapes[2]:
             if x[0]=='efficientnet': 
                 if self.model_type=='efficientnet-b2':
